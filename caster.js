cecd829f85b7e1a298f8486cb1c7660f61e775b8
vaibhav sharma <waxvapour@gmail.com>
2017-09-28 18:53:31 +0530

    hee
    done

---
+++ caster.js
@@ -0,0 +1,16 @@
+/*
+Idiot_idea__caster 
+by, Waxvapour@gmail.com
+*/
+function hee()
+{
+if(pas.vaa.value == "waxvapour")
+{
+document.getElementById('captionaa').style.display="none";
+document.getElementById('captionbb').style.display="block";
+}
+else
+{
+	alert("wrong person");
+}
+}
